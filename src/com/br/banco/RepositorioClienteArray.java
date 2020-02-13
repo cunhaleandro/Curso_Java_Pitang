e0d44430934ddfb33fdf4aefae7308a3ab75ce6f
cunhaleandrojrc <leandro.cunha.ufpe@gmail.com>
2020-02-13 16:51:29 -0300

    Criação de metodos na classe "ReositórioClienteArray".

--- src/com/br/banco/RepositorioClienteArray.java
+++ src/com/br/banco/RepositorioClienteArray.java
@@ -1,28 +1,52 @@
 package com.br.banco;
 
-public class RepositorioClienteArray {
-private Cliente[] clientes;
-private int indice = 0;
-private static final int TAMANHO_CACHE = 100;
+import java.util.Arrays;
 
-public RepositorioClienteArray() {
-	clientes = new Cliente[TAMANHO_CACHE]; 
-}
-public void inserir(Cliente c) {
-	indice++;
-	clientes[indice] = c;
-}
-public int procurarIndice(String cpf) {
-	for(int i = 0; i<=indice; i++) {
-		if(clientes[i].getCpf().equals(cpf)) {
-			return i;
-		}
+public class RepositorioClienteArray {
+	private Cliente[] clientes;
+	private int indice = 0;
+	private static final int TAMANHO_CACHE = 100;
+
+	public RepositorioClienteArray() {
+		clientes = new Cliente[TAMANHO_CACHE];
 	}
-	return -1;
-}
-public void getInfos() {
-	for(int i = 0; i<=indice; i++) {
+
+	public void inserir(Cliente c) {
+		indice++;
+		clientes[indice] = c;
+	}
+
+	public int procurarIndice(String cpf) {
+		for (int i = 0; i <= indice; i++) {
+			if (clientes[i].getCpf().equals(cpf)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	public boolean existe(String cpf ) {
+		boolean resposta = false;
+		int i = procurarIndice(cpf);
+		if(i != -1) {
+			resposta = true;
+			
+		}
+		return resposta;
+	}
+	public Cliente procurar(String cpf) {
+		Cliente c = null;
+		if(existe(cpf)) {
+			int i = procurarIndice(cpf);
+			c = clientes[i];		
+			}else {
+				System.out.println("Cliente não encontrado!");
+			}
+		return c;
+	}
+	public void getInfos() {
+		for (int i = 0; i <= indice; i++) {
+			
+		}
 	
 	}
-}
 }
